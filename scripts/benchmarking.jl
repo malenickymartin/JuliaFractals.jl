c4649eac1361ce0f8e657fca3f0cde68684bf500
malenma3 <malenicky.martin@gmail.com>
2025-01-30 21:45:58 +0100

    benchamrking example

--- scripts/benchmarking.jl
+++ scripts/benchmarking.jl
@@ -1,23 +1,50 @@
 using SPJFractals
 using BenchmarkTools
 
-img_size = [1080, 720]
-center = [0.0, 0.0]
-plane_size = [4.0, 4.0 * 720 / 1080]
-iterations = 100
-radius = 0.7887
-fps = 100
-step_size = pi/180
-julia_closure = fractal_closure(julia_equation, img_size, true)
-
-tau = 0.0
-c_x = Observable(0.0)
-c_y = Observable
-img = @lift(transpose(julia_closure([iterations, $c_x, $c_y], center, plane_size)))
-heatmap!(img)
-
-@benchmark begin
-    tau = (tau + step_size) % (2 * pi)
-    c_x[] = radius * cos(tau)
-    c_y[] = radius * sin(tau)
+function naive_mandelbrot(img_size)
+    img = zeros(img_size...)
+    for i in 1:img_size[1]
+        for j in 1:img_size[2]
+            x = 2.0 * (i - 1) / img_size[1] - 1.0
+            y = 2.0 * (j - 1) / img_size[2] - 1.0
+            c = ComplexF64(x, -y)
+            z = ComplexF64(0, 0)
+            iter = UInt16(0)
+            while abs2(z) <= 4.0f0 && iter < 255
+                z = z * z + c
+                iter += 1
+            end
+            img[j, i] = iter
+        end
+    end
+    return img
 end
+
+# Benchmark the GPU and CPU implementations on image size 500x500
+println("GPU Img Size: 500x500")
+display(@benchmark compute_fractal(mandelbrot_equation, [255], [0.0, 0.0], [2.0, 2.0], [500, 500], true))
+println("CPU Img Size: 500x500")
+display(@benchmark compute_fractal(mandelbrot_equation, [255], [0.0, 0.0], [2.0, 2.0], [500, 500], false))
+println("Naive Img Size: 500x500")
+display(@benchmark naive_mandelbrot([500, 500]))
+
+# Benchmark the GPU and CPU implementations on image size 1000x1000
+println("GPU Img Size: 1000x1000")
+display(@benchmark compute_fractal(mandelbrot_equation, [255], [0.0, 0.0], [2.0, 2.0], [1000, 1000], true))
+println("CPU Img Size: 1000x1000")
+display(@benchmark compute_fractal(mandelbrot_equation, [255], [0.0, 0.0], [2.0, 2.0], [1000, 1000], false))
+println("Naive Img Size: 1000x1000")
+display(@benchmark naive_mandelbrot([1000, 1000]))
+
+# Benchmark the GPU and CPU implementations on image size 1500x1500
+println("GPU Img Size: 1500x1500")
+display(@benchmark compute_fractal(mandelbrot_equation, [255], [0.0, 0.0], [2.0, 2.0], [1500, 1500], true))
+println("CPU Img Size: 1500x1500")
+display(@benchmark compute_fractal(mandelbrot_equation, [255], [0.0, 0.0], [2.0, 2.0], [1500, 1500], false))
+println("Naive Img Size: 1500x1500")
+display(@benchmark naive_mandelbrot([1500, 1500]))
+
+
+
+
+
